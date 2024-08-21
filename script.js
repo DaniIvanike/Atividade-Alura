7504b2250e9555f38ae9b2ea04dc61250b5b1b3d
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-08-21 07:33:26 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const 
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPrincipal = document.querySelector('caixa-perguntas');
+const caixaPrincipal = document.querySelector('caixa-alternativas');
+const caixaPrincipal = document.querySelector('caixa-resultado');
+const caixaPrincipal = document.querySelector('texto-resultado');
