72dcd2a1e72680881ec69b0ab525c072087411af
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-08-21 08:18:13 -0300

    .

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-const caixaPrincipal = document.querySelector('caixa-principal');
-const caixaPerguntas = document.querySelector('caixa-perguntas');
-const caixaAlternativas = document.querySelector('caixa-alternativas');
-const caixaResultado = document.querySelector('caixa-resultado');
-const textoResultado = document.querySelector('texto-resultado');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const alternativas = [
     {
         enunciado: "No âmbito social, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 1",
+                texto: "Altenativa 1",
                 afirmacao: "Afirmação 1",
             },
             {
-                texto: "Alternativa 2",
+                texto: "Altenativa 2",
                 afirmacao: "Afirmação 2",
             },
         ]
     },
-
     {
         enunciado: "No âmbito ambiental, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 3",
+                texto: "Altenativa 3",
                 afirmacao: "Afirmação 3",
             },
             {
-                texto: "Alternativa 4",
+                texto: "Altenativa 4",
                 afirmacao: "Afirmação 4",
             },
         ]
     },
-
     {
-        enunciado:" No âmbito tecnológico, você prefere:",
+        enunciado: "No âmbito tecnológico, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 5",
+                texto: "Altenativa 5",
                 afirmacao: "Afirmação 5",
             },
             {
-                texto: "Altrenativa 6",
+                texto: "Altenativa 6",
                 afirmacao: "Afirmação 6",
             },
         ]
@@ -62,7 +60,7 @@ function mostraAlternativas(){
     for (const opcao of perguntaAtual.alternativa) {
         const botaoAlternativa = document.createElement('button');
         botaoAlternativa.textContent = opcao.texto;
-        caixaAlternativas.appendChild(botaoAlternativa); 
+        caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
 
