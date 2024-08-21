05a9f275153c6e9d8dff5f568b2df97a905f27e1
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-08-21 07:43:37 -0300

    .

--- script.js
+++ script.js
@@ -5,46 +5,46 @@ const caixaPrincipal = document.querySelector('caixa-resultado');
 const caixaPrincipal = document.querySelector('texto-resultado');
 
 const alternativas = [
-{
-    enunciado: "No âmbito social, você prefere:",
-    alternativa: [
-        {
-            texto: "Altrnativa 1",
-            afirmacao: "Afirmação 1",
-        },
-        {
-            texto: "Altrnativa 2",
-            afirmacao: "Afirmação 2",
-        },
-    ]
-},
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativa: [
+            {
+                texto: "Altrnativa 1",
+                afirmacao: "Afirmação 1",
+            },
+            {
+                texto: "Altrnativa 2",
+                afirmacao: "Afirmação 2",
+            },
+        ]
+    },
 
-{
-    enunciado: "No âmbito ambiental, você prefere:",
-    alternativa: [
-        {
-            texto: "Altrnativa 3",
-            afirmacao: "Afirmação 3",
-        },
-        {
-            texto: "Altrnativa 4",
-            afirmacao: "Afirmação 4",
-        },
-    ]
-},
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativa: [
+            {
+                texto: "Altrnativa 3",
+                afirmacao: "Afirmação 3",
+            },
+            {
+                texto: "Altrnativa 4",
+                afirmacao: "Afirmação 4",
+            },
+        ]
+    },
 
-{
-    enunciado:" No âmbito tecnológico, você prefere:",
-    alternativa: [
-        {
-            texto: "Altrnativa 5",
-            afirmacao: "Afirmação 5",
-        },
-        {
-            texto: "Altrnativa 6",
-            afirmacao: "Afirmação 6",
-        },
-    ]
-},
+    {
+        enunciado:" No âmbito tecnológico, você prefere:",
+        alternativa: [
+            {
+                texto: "Altrnativa 5",
+                afirmacao: "Afirmação 5",
+            },
+            {
+                texto: "Altrnativa 6",
+                afirmacao: "Afirmação 6",
+            },
+        ]
+    },
 ]
 
