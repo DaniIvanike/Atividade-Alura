5d1ce62630847efd376250d576a22cfe03364639
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-08-28 08:19:45 -0300

    mn

--- script.js
+++ script.js
@@ -46,6 +46,54 @@ const alternativas = [
     },
 ]
 
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const alternativas = [
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativa: [
+            {
+                texto: "Altenativa 1",
+                afirmacao: "Afirmação 1",
+            },
+            {
+                texto: "Altenativa 2",
+                afirmacao: "Afirmação 2",
+            },
+        ]
+    },
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativa: [
+            {
+                texto: "Altenativa 3",
+                afirmacao: "Afirmação 3",
+            },
+            {
+                texto: "Altenativa 4",
+                afirmacao: "Afirmação 4",
+            },
+        ]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativa: [
+            {
+                texto: "Altenativa 5",
+                afirmacao: "Afirmação 5",
+            },
+            {
+                texto: "Altenativa 6",
+                afirmacao: "Afirmação 6",
+            },
+        ]
+    },
+]
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = ""; 
